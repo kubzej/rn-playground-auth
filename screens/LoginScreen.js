b48250d990a5f7dcc60b6fa748ab3ee8d9d90b09
Jakub Mares <jakub.mares@heidelbergcement.com>
2022-12-10 11:26:55 +0100

    error handling

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,13 +2,22 @@ import { useState } from 'react';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
 import AuthContent from '../components/Auth/AuthContent';
 import { login } from '../util/auth';
+import { Alert } from 'react-native';
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
   async function loginHandler({ email, password }) {
     setIsAuthenticating(true);
-    await login(email, password);
+    try {
+      await login(email, password);
+    } catch (err) {
+      Alert.alert(
+        'Authentication failed!',
+        'Could not log you in, please check your credentials or try again later.'
+      );
+    }
+
     setIsAuthenticating(false);
   }
 
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import { Alert } from 'react-native';
 import AuthContent from '../components/Auth/AuthContent';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
 import { createUser } from '../util/auth';
@@ -9,12 +9,16 @@ function SignupScreen() {
 
   async function signupHandler({ email, password }) {
     setIsAuthenticating(true);
-    await createUser(email, password);
+    try {
+      await createUser(email, password);
+    } catch (error) {
+      Alert.alert('Sign up failed', 'Plese try again later');
+    }
     setIsAuthenticating(false);
   }
 
   if (isAuthenticating) {
-    return <LoadingOverlay message="Creating user..." />;
+    return <LoadingOverlay message='Creating user...' />;
   }
 
   return <AuthContent onAuthenticate={signupHandler} />;
