ee6eebabab298b4be47b8bd4abff55e9d5983f20
Jakub Mares <jakub.mares@heidelbergcement.com>
2022-12-10 14:25:22 +0100

    get data only is user is authenticated

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,32 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState, useContext } from 'react';
+import axios from 'axios';
+import { AuthContext } from '../store/auth-context';
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState('');
+
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  // get data only if you are authenticated
+  // could be as helper file
+  useEffect(() => {
+    axios
+      .get(
+        'https://rn-playground-expense-tracker-default-rtdb.firebaseio.com/message.json?auth=' +
+          token
+      )
+      .then((response) => {
+        setFetchedMessage(response.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
